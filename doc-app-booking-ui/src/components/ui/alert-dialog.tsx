8b152306782837b915246ebf89377795d112cc18
vimal <vimalraj612@gmail.com>
2025-12-07 10:34:07 +0400

    sainath - fix

--- doc-app-booking-ui/src/components/ui/alert-dialog.tsx
+++ doc-app-booking-ui/src/components/ui/alert-dialog.tsx
@@ -84,7 +84,7 @@ function AlertDialogFooter({
     <div
       data-slot="alert-dialog-footer"
       className={cn(
-        "flex flex-col-reverse gap-2 sm:flex-row sm:justify-end",
+        "flex flex-row gap-2 justify-center sm:justify-end",
         className,
       )}
       {...props}
